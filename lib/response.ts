32b40aa8c0b47c31f56ee0a4e94dad8b97e0f67e
Jack Olivieri <jolivieri@anaconda.com>
2026-01-27 16:51:26 +0100

    add missing files

---
+++ lib/response.ts
@@ -0,0 +1,14 @@
+/// <reference path="../html.d.ts" />
+import okHtml from '../html/index.html'
+import notFoundHtml from '../html/404.html'
+
+export const htmlResponse = (content: string): Response =>
+	new Response(okHtml, {
+		headers: { 'content-type': 'text/html' },
+	})
+
+export const notFoundResponse = (): Response =>
+	new Response(notFoundHtml, {
+		headers: { 'content-type': 'text/html' },
+		status: 404,
+	})
